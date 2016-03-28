e95d46b3c9f587ed03a393b130c9391118bd18fc
Oleg Dreyman <dreymonde@me.com>
2016-03-28 19:41:32 +0300

    Vocabulaire dependency removed since it's available in InterchangeVoc

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "VocabulaireLinux",
     dependencies: [
-    	.Package(url: "https://github.com/QuStudio/Vocabulaire.git", majorVersion: 0, minor: 1),
     	.Package(url: "https://github.com/QuStudio/InterchangeVoc.git", majorVersion: 0, minor: 1)
     ]
 )
