752f2b7552c5f77df1b02a5bf4ece96a2e65623c
Oleg Dreyman <dreymonde@me.com>
2016-04-30 21:38:00 +0300

    Vocabulaire 0.4

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "VocabulaireLib",
     dependencies: [
-    	.Package(url: "https://github.com/QuStudio/StructuredVoc.git", majorVersion: 0, minor: 2)
+    	.Package(url: "https://github.com/QuStudio/StructuredVoc.git", majorVersion: 0, minor: 3),
     ]
 )
